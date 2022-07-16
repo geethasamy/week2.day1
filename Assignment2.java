b9efcac2f96cbbd8994bb9a081ffe6c5e3d7408d
geethasamy <70588566+geethasamy@users.noreply.github.com>
2022-07-17 00:33:44 +0800

    Add files via upload
    Week2_day1_Selenium assignments

---
+++ Assignment2.java
@@ -0,0 +1,66 @@
+package week2.day1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Assignment2 {
+
+	public static void main(String[] args) {
+		
+		//Duplicate Lead
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.partialLinkText("Create")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("csw");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Geetha");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Periyasamy");
+		driver.findElement(By.id("createLeadForm_firstNameLocal")).sendKeys("Gee");
+		driver.findElement(By.name("departmentName")).sendKeys("IT");
+		driver.findElement(By.name("description")).sendKeys("This is a demo project");
+		driver.findElement(By.id("createLeadForm_primaryEmail")).sendKeys("geetha@gmail.com");
+		
+		WebElement dropdown1 = driver.findElement(By.id("createLeadForm_generalStateProvinceGeoId"));
+		Select select = new Select(dropdown1);
+		select.selectByVisibleText("New York");
+		driver.findElement(By.name("submitButton")).click();
+		String str = driver.getTitle();
+		if(str.equalsIgnoreCase("View Lead | opentaps CRM"))
+		{
+			System.out.println("First Time Verified the Tiltle successfully "
+					+ "\n" + str);
+		}
+		else
+		{
+			System.out.println("Wrong Title" + str);
+		}
+		
+		driver.findElement(By.partialLinkText("Duplicate")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).clear();
+		driver.findElement(By.id("createLeadForm_firstName")).clear();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Gain");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Samy");
+		driver.findElement(By.name("submitButton")).click();
+		if(str.equalsIgnoreCase("View Lead | opentaps CRM"))
+		{
+			System.out.println("Second Time Verified the Tiltle successfully "
+					+ "\n" + str);
+		}
+		else
+		{
+			System.out.println("Wrong Title" + str);
+		}
+
+	}
+
+}
